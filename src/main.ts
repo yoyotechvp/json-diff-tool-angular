6882c2e9c4aecd4e771abfdec4843fe1bf120d20
yoyotechvp <yoyotech@aliyun.com>
2026-04-16 21:05:05 +0800

    feat: 第二轮输出
    结果不满意
    1. 分屏视图左右两侧一样的效果，没有达到分屏的效果
    2. 全部展开/折叠和根节点的展开/折叠无效
    3. 测试命令 npm run test 无法执行
    4. 没有大 json 的示例

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import { platformBrowser } from '@angular/platform-browser';
-import { AppModule } from './app/app-module';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { App } from './app/app';
+import { provideRouter } from '@angular/router';
 
-platformBrowser().bootstrapModule(AppModule, {
-  
-})
-  .catch(err => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    provideRouter([])
+  ]
+}).catch(err => console.error(err));
